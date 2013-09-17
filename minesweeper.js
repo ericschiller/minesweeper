a239c0713b93274bb501e180427c80247f1b7b37
Eric Schiller <ericschiller@gmail.com>
2013-09-17 10:20:01 -0700

    rec

--- minesweeper.js
+++ minesweeper.js
@@ -7,24 +7,15 @@ var ms = {
     for( i = 1; i <= size; i++){
       for( k = 1; k <= size; k++){
         this.grid.push(new Tile(k, i));
-        $('#grid').append('<div class="tile" id="' + i + '_' + k + '"></div>').css('width', this.grid_size * 30);
+        $('#grid').append('<div class="tile" id="' + k + '_' + i + '"></div>').css('width', this.grid_size * 30);
 
       }
     }
     $('#grid .tile').click(function(){
       var t = $(this).attr('id').split('_');
+      console.log(this);
       temp_tile = ms.tile_find(parseInt(t[0]), parseInt(t[1]));
-      if (temp_tile.mine){
-        $(this).css('background-color', 'red').text('☼');
-      }
-      else{
-        var mine_count = temp_tile.find_mines();
-        $(this).css('background-color', '#E6E6E6');
-        if (mine_count > 0){
-          $(this).text(mine_count);
-        }
-      }
-
+      temp_tile.find_mines();
     });
 
 
@@ -66,19 +57,51 @@ var Tile = function(x, y) {
     this.x = x;
     this.y = y;
     this.mine = false;
+    this.checked = false;
+    this.id = '#' + this.x + '_' + this.y;
     this.find_mines = function(){
-      var count = 0;
+      if (this.mine){
+        $(this.id).css('background-color', 'red').text('☼');
+      }
+      else{
+        $(this.id).css('background-color', '#E6E6E6');
+        this.checked = true;
+        var count = 0;
+        var _x = [this.x, this.x + 1, this.x - 1];
+        var _y = [this.y, this.y + 1, this.y - 1];
+        for( i = 0; i < _x.length; i++){
+          for( j = 0; j < _y.length; j++){
+            var tile = ms.tile_find(_x[i], _y[j]);
+            if (tile != undefined && tile != this && this.checked != true){
+              tile.find_mines();
+              count +=(tile.mine ? 1 : 0);
+            }
+          }
+        }
+        if (count > 0){
+          $(this.id).text(count);
+        }
+        else{
+        }
+      }
+    };
+    this.around = function(){
       var _x = [this.x, this.x + 1, this.x - 1];
       var _y = [this.y, this.y + 1, this.y - 1];
-      for( i = 0; i < _x.length; i++){
-        for( j = 0; j < _y.length; j++){
-          var tile = ms.tile_find(_x[i], _y[j]);
-          if (tile != undefined && tile != this){
-            count +=(tile.mine ? 1 : 0);
+      console.log('start ' + this.x + "  " + this.y);
+      var to_check = [];
+      for( k = 0; k < _x.length; k++){
+        for( l = 0; l < _y.length; l++){
+          console.log(_x[k] + "  " + _y[l]);
+          var tile = ms.tile_find(_x[k], _y[l]);
+          if (tile != undefined && tile.checked === false ) {
+            to_check.push(tile);
           }
         }
       }
-      return count;
+      $.each(to_check, function(thing){
+        to_check[thing].find_mines();
+      });
     }
 };
 
